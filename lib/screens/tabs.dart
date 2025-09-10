bf8712066456faea47169a0e4f74f0dd0d08fe3d
i-Abdulhaseeb <abdulhaseeb2894@gmail.com>
2025-09-10 17:34:10 +0500

    Side drawer + filter page added

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,8 @@
 import 'package:cooking_app/models/meal.dart';
 import 'package:cooking_app/screens/categories.dart';
+import 'package:cooking_app/screens/filters.dart';
 import 'package:cooking_app/screens/meals.dart';
+import 'package:cooking_app/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -20,12 +22,36 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message, style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+
   void toggleMealFavouriteStatus(Meal meal) {
     final isExisting = _favouriteMeals.contains(meal);
     if (isExisting) {
-      _favouriteMeals.remove(meal);
+      setState(() {
+        _showInfoMessage(meal.title + " is no longer favourite");
+        _favouriteMeals.remove(meal);
+      });
     } else {
-      _favouriteMeals.add(meal);
+      setState(() {
+        _showInfoMessage(meal.title + " is added to favourite");
+        _favouriteMeals.add(meal);
+      });
+    }
+  }
+
+  void _setScreen(String identifier) {
+    Navigator.of(context).pop();
+    if (identifier == 'filters') {
+      Navigator.of(
+        context,
+      ).push(MaterialPageRoute(builder: (ctx) => FiltersScreen()));
     }
   }
 
@@ -44,6 +70,7 @@ class _TabsScreenState extends State<TabsScreen> {
     }
     return Scaffold(
       appBar: AppBar(title: Text(activePageTitle)),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
